fd1ffccb0aae7e9abfdd2db5fc1fcf282922060d
NosliwKuns <quispealaya73@gmail.com>
2024-03-19 16:53:18 -0500

    Add more styles to the page login

--- eiich/frontend/src/components/Input.tsx
+++ eiich/frontend/src/components/Input.tsx
@@ -1,8 +1,17 @@
-import { ChangeEvent, useEffect, useState } from "react"
+import { Login } from "@/pages/login/Login"
+import { ChangeEvent, Dispatch, FC, SetStateAction, useState } from "react"
+import { FaEye, FaEyeSlash } from "react-icons/fa"
 
-export const Input = () => {
+interface Props {
+	label: string
+	type: string
+	value: string | number
+	setValue: Dispatch<SetStateAction<Login>>
+}
+
+export const Input: FC<Props> = ({ label, type, value, setValue }) => {
 	const [isFocus, setIsFocus] = useState<boolean>(false)
-	const [value, setValue] = useState<string>("")
+	const [isView, setIsView] = useState<boolean>(false)
 
 	const handleOnFocus = () => {
 		setIsFocus(true)
@@ -14,40 +23,63 @@ export const Input = () => {
 
 	const handleOnchange = (event: ChangeEvent<HTMLInputElement>) => {
 		const { value } = event.target
-		setValue(value)
+		setValue((prevValue) => ({
+			...prevValue,
+			[type]: value,
+		}))
 	}
 
-	// useEffect(() => {
-	// 	if (value) {
-	// 		setIsFocus(true)
-	// 	}
-	// }, [value])
-	// console.log(isFocus)
+	const handleOnClick = () => {
+		setIsView(!isView)
+	}
+
 	return (
-		<div className="relative h-[52px]">
-			<label
-				htmlFor="email"
-				className={`absolute left-[14px] ${isFocus ? "top-[2.5px] text-[12px] text-[#485585]/80" : "top-1/2 text-[#485585]/70 text-sm"} -translate-y-1/2 font-semibold px-2 z-30 transition-all`}
-				style={{
-					background: `linear-gradient(to bottom, ${isFocus ? "white 50%" : "#e8f0fe 90%"}, #e8f0fe 20%)`
-				}}
-				// style={{
-				// 	mixBlendMode: 'multiply' // Elige el modo de fusión deseado
-				// }}
+		<div className="relative top-0 h-[52px]">
+			<div
+				className={`relative top-1/2 left-[14px] -translate-y-1/2 w-fit font-semibold px-2 z-30 pointer-events-none text-nowrap transition-all`}
 			>
-				Correo electronico*
-			</label>
-			<div className="h-full">
-				{isFocus && <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full bg-gradient-to-br to-electric-violet-500 from-vividIndigo z-10 rounded-[10px]">aaa</span>}
+				<label
+					htmlFor={type}
+					className={`relative ${
+						isFocus ? "text-xs text-[#485585]/60 -top-[26px]" : "text-sm text-[#485585]/40 top-0"
+					} z-20 transition-all duration-500`}
+				>
+					{label}
+				</label>
+				<div
+					className={`absolute -top-[26px] left-0 ${
+						isFocus ? "w-full" : "w-0"
+					} h-full transition-all duration-[400ms]`}
+					style={{
+						background: `linear-gradient(to bottom, white 50%, #e8f0fe 50%)`,
+					}}
+				></div>
+			</div>
+			<div className="h-full -mt-6">
+				{isFocus && (
+					<span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full bg-gradient-to-br to-electric-violet-500 from-vividIndigo z-10 rounded-[10px]"></span>
+				)}
 				<input
-					id="email"
-					type="email"
-					className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${isFocus ? "w-[calc(100%-5px)] h-[calc(100%-5px)] bg-white" : "w-full h-full"} bg-[#e8f0fe] px-5 text-sm font-medium outline-none rounded-lg z-20`}
+					id={type}
+					type={type === "password" && isView ? "text" : type}
+					name={type}
+					className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${
+						isFocus ? "w-[calc(100%-5px)] h-[calc(100%-5px)]" : "w-full h-full"
+					} bg-zumthor-100 px-5 text-sm font-medium outline-none rounded-lg z-20`}
 					onFocus={handleOnFocus}
 					onBlur={handleBlur}
 					value={value}
 					onChange={handleOnchange}
 				/>
+				{type === "password" && (
+					<button onClick={handleOnClick} type="button">
+						{isView ? (
+							<FaEyeSlash className="absolute top-1/2 right-[11.5px] -translate-y-1/2 z-40 text-gray-500" size={18} />
+						) : (
+							<FaEye className="absolute top-1/2 right-3 -translate-y-1/2 z-40 text-gray-500" />
+						)}
+					</button>
+				)}
 			</div>
 		</div>
 	)
--- eiich/frontend/src/pages/login/Login.tsx
+++ eiich/frontend/src/pages/login/Login.tsx
@@ -1,108 +1,75 @@
 import { Input } from "@/components/Input"
 import { Buttom } from "@/components/ui/Buttom"
 import { Typography } from "@/components/ui/Typography"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { IoArrowBack } from "react-icons/io5"
+
+export interface Login {
+	email: string
+	password: string
+}
 
 export const Login = () => {
+	const [value, setValue] = useState<Login>({
+		email: "",
+		password: "",
+	})
+
 	return (
-		<div>
-			<div className="flex bg-babyBlue min-h-screen rounded-lg py-28">
-				<div className="m-auto p-12 bg-white w-fit rounded-2xl">
-					<div className="flex items-center xl:p-10">
-						<form className="flex flex-col w-full h-full pb-6 text-center bg-white rounded-3xl">
-							<Typography className="mb-6 font-extrabold text-dark-grey-900">
-								Iniciar sesión
-							</Typography>
-							{/* <label
-								htmlFor="email"
-								className="mb-2 text-sm text-start text-gray-700"
-							>
-								Correo electronico*
-							</label>
-							<input
-								id="email"
-								type="email"
-								placeholder="Ingresa tu correo"
-								className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-gray-400 mb-7 placeholder:text-gray-700 bg-gray-200 text-dark-gray-900 rounded-2xl"
-							/>
-							<label
-								htmlFor="password"
-								className="mb-2 text-sm text-start text-grey-900"
-							>
-								Contraseña*
-							</label>
-							<input
-								id="password"
-								type="password"
-								placeholder="Ingresa tu contraseña"
-								className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-gray-400 placeholder:text-gray-700 bg-gray-200 text-dark-gray-900 rounded-2xl"
-							/> */}
-							<input type="text" name="" id="" />
-							<label
-								htmlFor="password"
-								className="mb-2 text-sm text-start text-grey-900"
-							>
-								Contraseña*
-							</label>
-							<input
-								id="password"
-								type="password"
-								placeholder="Ingresa tu contraseña"
-								className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium focus:bg-gray-400 placeholder:text-gray-700 bg-gray-200 text-dark-gray-900 rounded-2xl"
-							/>
-							<Input />
-							<div className="flex flex-row justify-between mb-8">
-								<label className="relative inline-flex items-center mr-3 cursor-pointer select-none">
-									<input
-										type="checkbox"
-										checked
-										value=""
-										className="sr-only peer"
+		<div className="relative flex bg-babyBlue min-h-screen rounded-lg py-28">
+			<Link
+				to="/"
+				className="absolute top-3 left-3 flex items-center gap-2 bg-white rounded-lg font-semibold py-2 px-4 border-2 text-elc-500 border-electric-violet-500"
+			>
+				<IoArrowBack /> Ir a inicio
+			</Link>
+			<div className="m-auto p-12 bg-white w-fit rounded-2xl">
+				<div className="flex items-center xl:p-10">
+					<form className="flex flex-col gap-6 w-full h-full text-center bg-white rounded-3xl">
+						<Typography className="mb-2 font-extrabold text-dark-grey-900">Iniciar sesión</Typography>
+
+						<Input label="Correo electrónico*" type="email" value={value.email} setValue={setValue} />
+
+						<Input label="Contraseña*" type="password" value={value.password} setValue={setValue} />
+
+						<div className="flex flex-row justify-between gap-6 -mt-2 text-xs items-center">
+							<label className="relative inline-flex items-center mr-3 cursor-pointer select-none">
+								<input type="checkbox" checked value="" className="sr-only peer" />
+								<div className="w-4 h-4 bg-white border-2 rounded-sm border-gray-500 peer peer-checked:border-0 peer-checked:bg-electric-violet-500">
+									<img
+										className=""
+										src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/icons/check.png"
+										alt="tick"
 									/>
-									<div className="w-5 h-5 bg-white border-2 rounded-sm border-gray-500 peer peer-checked:border-0 peer-checked:bg-purple-500">
-										<img
-											className=""
-											src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/icons/check.png"
-											alt="tick"
-										/>
-									</div>
-									<span className="ml-3 text-sm font-normal text-gray-900">
-										Mantenerme conectado
-									</span>
-								</label>
-								<a
-									href="javascript:void(0)"
-									className="mr-4 text-sm font-medium text-purple-blue-500"
-								>
-									Olvidaste la contraseña?
-								</a>
-							</div>
-							<Buttom className="w-full py-4">
-								Iniciar sesión
-							</Buttom>
-							<div className="flex items-center my-3">
-								<hr className="h-0 border-b border-solid border-gray-200 grow" />
-								<p className="mx-4 text-grey-600">o</p>
-								<hr className="h-0 border-b border-solid border-gray-200 grow" />
-							</div>
-							<a className="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 cursor-pointer rounded-lg text-gray-700 bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:ring-grey-300">
-								<img
-									className="h-5 mr-2"
-									src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
-									alt=""
-								/>
-								Iniciar sesión con Google
+								</div>
+								<span className="ml-2 font-normal text-gray-900">Mantenerme conectado</span>
+							</label>
+							<a href="#" className="font-medium text-electric-violet-500 cursor-pointer">
+								¿Olvidaste la contraseña?
 							</a>
-							<p className="text-sm leading-relaxed text-grey-900">
-								Aún no estas suscrito?{" "}
-								<a
-									href="javascript:void(0)"
-									className="font-bold text-gray-500"
-								>
-									Crear cuenta
-								</a>
-							</p>
-						</form>
-					</div>
+						</div>
+						<Buttom className="w-full py-4">Iniciar sesión</Buttom>
+						<div className="flex items-center -my-3">
+							<hr className="h-0 border-b border-solid border-gray-200 grow" />
+							<p className="mx-4 text-grey-600">ó</p>
+							<hr className="h-0 border-b border-solid border-gray-200 grow" />
+						</div>
+						<a className="flex items-center justify-center w-full py-4 mb-2 text-sm font-medium transition duration-300 cursor-pointer rounded-lg text-gray-700 bg-zumthor-100 hover:bg-zumthor-50 focus:ring-4 focus:ring-grey-300">
+							<img
+								className="h-5 mr-2"
+								src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
+								alt=""
+							/>
+							Iniciar sesión con Google
+						</a>
+						<p className="text-sm leading-relaxed text-grey-900">
+							¿Aún no estas suscrito?{" "}
+							<Link to="/registro" className="font-bold text-electric-violet-500">
+								Suscribirse
+							</Link>
+						</p>
+					</form>
 				</div>
 			</div>
 		</div>
